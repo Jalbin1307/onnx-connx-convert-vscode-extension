d0cc0e4403ee2e9cd75653230dc4445c2f06e723
Jalbin1307 <az13077@gmail.com>
2021-12-22 11:08:06 +0900

    feat : Add activitybar(connx)

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,12 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	console.log('Congratulations, your extension "onnx2connx-convert" is now active!');
 
+	const provide = new OnnxViewProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(OnnxViewProvider.viewType, provide),
+
+	);
+
 	// context menu command : ONNX to CONNX convert
 	let disposable = vscode.commands.registerCommand('onnx2connx-convert.convert', (uri: Uri, items : Uri[]) => {
 		var formData = new FormData();
@@ -41,3 +47,44 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {}
+
+
+class OnnxViewProvider implements vscode.WebviewViewProvider {
+
+	public static readonly viewType = 'opw';
+
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
+			
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			
+			enableScripts: true,
+
+			localResourceRoots: [
+				this._extensionUri
+			]
+		};
+
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+	}
+
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'src', 'page.html'));
+		const pathUri = scriptUri.with({scheme:'vscode-resource'});
+		const html = fs.readFileSync(pathUri.fsPath,'utf-8');
+		return html;
+	}
+}
